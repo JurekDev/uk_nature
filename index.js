a2d1791e303bf5d7de76aae7c5c4375113216616
JurekDev <jurek.mueller@outlook.de>
2017-07-31 09:25:12 +0200

    added phone and email data

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var data=[
     description:"arnside and silverdale is an area between lancashire and cumbria. it covers 29 square miles and was designated in 1972. the area is characterised by the two carboniferous limestone hills named arnside knott and warton crag. the coastal areas contain large salt marsh areas.",
     region:"north west",
     email:"lucy@arnsidesilverdaleaonb.org.uk",
-    phone:"01524, 761034",
+    phone:"01524 761034",
     image:"https://upload.wikimedia.org/wikipedia/commons/2/2a/Arnside123.jpg"
   },
   {
@@ -29,7 +29,7 @@ var data=[
     description:"the forest of bowland, also knowwn as the bowland fells, is an area of barren gritstone fells, deep valleys and peat moorland. it is located in north-east lancashire and was designated in 1964. the region was once described as the switzerland of england because of its ancient wilderness and the highets point ward's stone, with 1841 feet.",
     region:"north west",
     email:"bowland@lancashire.gov.uk",
-    phone:"01200, 448000",
+    phone:"01200 448000",
     image:"https://upload.wikimedia.org/wikipedia/commons/5/5f/Ward%27s_Stone.jpg"
   },
   {
@@ -44,8 +44,8 @@ var data=[
     areaName:"cannock chase",
     description:"the cannok chase is a former royal forest in the county of staffordshire. it covers 26 square miles and was designated in 1958. there is a herd of 800 fallow deers and the marquis drive visitor's centre with a bird feeding station.",
     region:"west midlands",
-    email:"avvvvddd",
-    phone:"avvddvd",
+    email:"ruth.hytch@staffordshire.gov.uk",
+    phone:"01889 882613",
     image:"https://upload.wikimedia.org/wikipedia/commons/2/26/Cannock_Chase_Path.jpg"
   },
   {
@@ -60,24 +60,24 @@ var data=[
     areaName:"shropshire hills",
     description:"the shropshire hills are located in the english county of shropshire, extending to its border with wales. it was designated in 1958 and covers an area of 310 square miles. the highest point is brown clee hill with 1772 feet. points of interest are the townbrook valley in the long mynd and the wrekin near wellington.",
     region:"west midlands",
-    email:"avcv",
-    phone:"aasx",
+    email:"phil.holden@shropshire.gov.uk",
+    phone:"01743 254740",
     image:"https://upload.wikimedia.org/wikipedia/commons/8/88/Shropshire_Long_Mynd.jpg"
   },
   {
     areaName:"malvern hills",
     description:"the malvern hills are a range of hills in the counties worcestershire and herefordshire. it was designated in 1959 and covers an area of 5 square miles. the malvern hills are known for their spring water initially made famous by the region's many holy wells.",
     region:"west midlands",
-    email:"assx",
-    phone:"asssaa",
+    email:"pesrich@worcestershire.gov.uk",
+    phone:"01684 560616",
     image:"https://upload.wikimedia.org/wikipedia/commons/3/3f/Malvern_Hills_-_England.jpg"
   },
   {
     areaName:"cotswolds",
     description:"the cotswolds is an area in south central england containing the cotswold hills, a range of rolling hills which rise from the meadows of the upper thames to an escarpment, named the cotswold edge. it covers 768 square miles and was designated in 1966.",
     region:"south west",
-    email:"savsvs",
-    phone:"svvdvsa",
+    email:"martin.lane@cotswoldsaonb.org.uk",
+    phone:"01451 862000",
     image:"https://upload.wikimedia.org/wikipedia/commons/e/e7/Castle_combe_cotswolds.jpg"
   },
   {
@@ -93,144 +93,144 @@ var data=[
     areaName:"cranborne chase and west wiltshire downs",
     description:"the cranborne chase is a chalk plateau in central southern england. it covers an area of 379 square miles and its highest point is win green, in wiltshire, at 910 feet. there are many nelithic and bronze age mounuments, including the henge monumnents at knowlton.",
     region:"south west",
-    email:"avsdvdvs",
-    phone:"avsdvvds",
+    email:"lindanunn@cranbornechase.org.uk",
+    phone:"01725 517417",
     image:"https://upload.wikimedia.org/wikipedia/commons/1/1b/Dorset_brings_01.jpg"
   },
   {
     areaName:"cornwall",
     description:"the cornwall area of outstanding beauty covers eleven coastlines and the bodmin moor. it covers 370 square miles in cornwall, england and was designated in 1959.",
     region:"south west",
-    email:"avsdvs",
-    phone:"avsdvds",
+    email:"info@cornwall.gov.uk",
+    phone:"01872 322350",
     image:"https://upload.wikimedia.org/wikipedia/commons/f/fa/Cape_Cornwall_%28Judithili%29_edit.jpg"
   },
   {
     areaName:"dorset",
     description:"dorset is an area in south west england. it covers an area of 1024 square miles. dorset's coastline is one of the most visited coastlines in the world because it shows, along the the course of 95 miles, rocks from the beginning of triassic, through the jurassic and up to the end of the cretaceous.",
     region:"south west",
-    email:"asdvds",
-    phone:"asvsds",
+    email:"dorsetaonb@dorsetcc.gov.uk",
+    phone:"01305 228239",
     image:"https://upload.wikimedia.org/wikipedia/commons/6/6c/Blue_lias_cliffs_at_Lyme_Regis.jpg"
   },
   {
     areaName:"east devon",
     description:"the east devon area of outstanding natural beauty is shaped by man's activities from prehistoric times to the present. it was designated in 1963. it includes the heritage coastal walk with beautiful valleys, woodland and the coastline of devon.",
     region:"south west",
-    email:"asdv",
-    phone:"bga",
+    email:"chris.woodruff@eastdevonaonb.org.uk",
+    phone:"01404 46663",
     image:"https://upload.wikimedia.org/wikipedia/commons/b/b7/Snow05_009.jpg"
   },
   {
     areaName:"isles of scilly",
     description:"the isles of scilly are an archipelago off the south western tip of the cornish peninsula. it covers 6,32 square miles and was designated 1975. the isles have the greatest density of scheduled monuments in the united kingdom.",
     region:"south west",
-    email:"vc a",
-    phone:"vfdya",
+    email:"jpearce@scilly.gov.uk",
+    phone:"01720 424317",
     image:"https://upload.wikimedia.org/wikipedia/commons/e/e7/Tresco_-_aerial_photo6_edit.jpg"
   },
   {
     areaName:"mendip hills",
     description:"the mendip hills is a range of limestone hills to the south of bristol and bath in somerset, england. it was designated in 1972 and covers 77 sqaure miles. there is evidence of mining in the mendip hills dating back to the bronze age.",
     region:"south west",
-    email:"ayfv",
-    phone:"ayfv",
+    email:"sjackson@somerset.gov.uk",
+    phone:"01761 463357",
     image:"https://upload.wikimedia.org/wikipedia/commons/d/dc/Crook_Peak_towards_Compton_Hill.jpg"
   },
   {
     areaName:"north devon coast",
     description:"the north devon coast is located in south england and contains a surprising diversity of scenery including tall rugged cliffs , wave cut platforms, sand dunes and traditional hedged fields with wind sculptured trees. it was designated in 1959 and covers an area of 66 square miles.",
     region:"south west",
-    email:"avvvvvvvvvvvvvv",
-    phone:"vya",
+    email:"jenny.carey-wood@devon.gov.uk",
+    phone:"01271 388758",
     image:"https://upload.wikimedia.org/wikipedia/commons/9/96/Cliffs_Clovelly_Coast_West.JPG"
   },
   {
     areaName:"north wessex downs",
     description:"the north wessex downs are located in the english counties  of west berkshire, hampshire, oxfordshire and wiltshire. the area is a site of scientific interest because of the dry valleys and sarsen outcrops. it was designated in 1972 and covers an area of 670 square miles.",
     region:"south west",
-    email:"avyxc",
-    phone:"xyca",
+    email:"henryoliver@northwessexdowns.org.uk",
+    phone:"01488 685440",
     image:"https://upload.wikimedia.org/wikipedia/commons/4/40/The_South_Western_Slopes_of_Walbury_Hill_-_geograph.org.uk_-_62332.jpg"
   },
   {
     areaName:"quantock hills",
     description:"the quantock hills is a range of hills west of bridgwater in somerset, with the highest point named wills neck with a height of 1262 feet. the area covers 38 square miles and was designated in 1970. the area includes the quantock village and parish churches.",
     region:"south west",
-    email:"ayxc",
-    phone:"ayxc",
+    email:"quantockhills@somerset.gov.uk",
+    phone:"01823 451884",
     image:"https://upload.wikimedia.org/wikipedia/commons/1/13/UplandQuantocks.jpg"
   },
   {
     areaName:"south devon",
     description:"the south devon area is located in the county of devon and contains a lot of rugged coastline. it also contains geological and geomorphological features and expansive panoramic views. it was designated in 1960 and covers an area of 130 square miles.",
     region:"south west",
-    email:"aycx",
-    phone:"vca",
+    email:"roger.english@southdevonaonb.org.uk",
+    phone:"01803 861435",
     image:"https://upload.wikimedia.org/wikipedia/commons/4/42/Slapton_Sands_1.jpg"
   },
   {
     areaName:"tamar valley",
     description:"the tamar valley is located in south west england and was formed by the river tamar. it was designated in 1963 and covers an area of 75 square miles. the river tamar is also designated as a unesco world heritage site.",
     region:"south west",
-    email:"ayxcv",
-    phone:"ayxc",
+    email:"info@tamarvalley.org.uk",
+    phone:"01822 835030",
     image:"https://upload.wikimedia.org/wikipedia/commons/6/67/River_Tamar%2C_near_Bohetherick_-_geograph.org.uk_-_977730.jpg"
   },
   {
     areaName:"northumberland coast",
     description:"the northumberland coast is covering 100 miles of coastline from berwick-upon_tweed to the river coquet estuary in the northeast of england. it was designated in 1958 and includes sandy beaches, rugged cliffs and isolated islands.",
     region:"north east",
-    email:"axyc",
-    phone:"ayxc",
+    email:"coastaonb@northumberland.gov.uk",
+    phone:"01670 533153",
     image:"https://upload.wikimedia.org/wikipedia/commons/e/e1/Bamburgh2006.jpg"
   },
   {
     areaName:"north pennines",
     description:"the north pennines are the northernmost section of the pennine range, which runs north south through northern england. the landscape is an open heather moors between deep dales, upland rivers, hay meadows and stone-built villages. some villages contain the legacies of a mining and industrial past. it was designated in 1988 and covers 770 square miles.",
     region:"north east",
-    email:"ayxx",
-    phone:"yvva",
+    email:"chris@northpenninesaonb.org.uk",
+    phone:"01388 528801",
     image:"https://upload.wikimedia.org/wikipedia/commons/f/f8/Cauldron_Snout_-_July_2006.jpg"
   },
   {
     areaName:"lincolnshire wolds",
     description:"the lincolnshire wolds is a range of hills in the county of lincolnshire. it was designated in 1973 and covers an area of 216 square miles. the lincolnshire wolds can be divided into four distinct areas. the main area of chalk hills in the north, the north west scarp, an area of ridges and valleys in the south west and the claylands in the south east.",
     region:"east midlands",
-    email:"ayvy",
-    phone:"vvvva",
+    email:"stephen.jack@lincolnshire.gov.uk",
+    phone:"01522 555780",
     image:"https://upload.wikimedia.org/wikipedia/commons/9/9f/Lincolnshirewolds.jpg"
   },
   {
     areaName:"dedham vale",
     description:"the deadham vale is an area on the essex suffolk border in east england. it supports a viable and diverse agriculture. the river stour is the key landscape focus and its course is defined by bank-side trees and wet meadows.",
     region:"east of england",
-    email:"abbbb",
-    phone:"axxx",
+    email:"dedhamvale.project@suffolk.gov.uk",
+    phone:"01394 445225",
     image:"https://upload.wikimedia.org/wikipedia/commons/f/f2/John_Constable_012.jpg"
   },
   {
     areaName:"norfolk coast",
     description:"the norfolk coast is located in norfolk, england and includes one of the country's finest dune systems, the winterton dunes. it was designated in 1968 and covers an are of 173 square miles.",
     region:"east of england",
-    email:"afds",
-    phone:"a",
+    email:"aonb@norfolk.gov.uk",
+    phone:"01328 850530",
     image:"https://upload.wikimedia.org/wikipedia/commons/3/3c/Cromer_beach_summer_UK.JPG"
   },
   {
     areaName:"suffolk coast and heaths",
     description:"the suffolk coast and heathhs is an area in suffolk england, which covers ancient woodland, commercial forestry, rivers and salt marsh. it was founded in 1970 and covers 60 miles of coast.",
     region:"east of england",
-    email:"adsf",
-    phone:"asfd",
+    email:"simon.amstutz@suffolk.gov.uk",
+    phone:"01394 445225",
     image:"https://upload.wikimedia.org/wikipedia/commons/b/b7/Dunwich_seafront_today.JPG"
   },
   {
     areaName:"chichester harbour",
     description:"chichester harbour is a large natural harbour to the south west of chichester. it covers 9,13 acres and was designated in 1970. it is one of britain's most popular boating waters with its wide expanses and intricrate creeks. the harbour is also a very popular area for bird watching.",
     region:"south east",
-    email:"asss",
-    phone:"aaaa",
+    email:"richard.austin@conservancy.co.uk",
+    phone:"01243 512301",
     image:"https://upload.wikimedia.org/wikipedia/commons/1/16/View_Across_Chichester_Harbour_-_geograph.org.uk_-_1717805.jpg"
   },
   //TODO Chiltern Hils
@@ -238,48 +238,48 @@ var data=[
     areaName:"chilterns",
     description:"the chiltern hills form a chalk escarpment in the south east of england. the highest point is haddington hill with 878 feet. the area covers 322 square miles and was designated in 1965.",
     region:"south east",
-    email:"vvva",
-    phone:"ssa",
+    email:"office@chilternsaonb.org",
+    phone:"01844 355500",
     image:"https://upload.wikimedia.org/wikipedia/commons/d/df/Chilterns_England_Rolling_Countryside.jpg"
   },
   {
     areaName:"high weald",
     description:"the high weald area extends across the counties of surrey, west sussex, east sussex and kent. it covers 560 square miles and was designated in 1980. it is characterised by an attractive, small-scale landscape conataining small farms and woodlands, historic parks and ridge-top villages.",
     region:"south east",
-    email:"asdff",
-    phone:"asdf",
+    email:"g.sherwin@highweald.org",
+    phone:"01580 879500",
     image:"https://upload.wikimedia.org/wikipedia/en/f/fa/Highwealdview.jpg"
   },
   {
     areaName:"isle of wight",
     description:"the isle of wight is england's largest offshore island. it was designated as an area of outstanding beauty in 1963 and covers 72 square miles.",
     region:"south east",
-    email:"sdfa",
-    phone:"sda",
+    email:"fiona.hanna@iow.gov.uk",
+    phone:"01983 823855",
     image:"https://upload.wikimedia.org/wikipedia/commons/7/7d/IsleOfWightFromTheISS.jpg"
   },
   {
     areaName:"kent downs",
     description:"the kent downs are the eastern half of the north downs and stretch from the london surrey borders to the white cliffs of dover. they cover 338 square miles and ar renowned for their natural beauty.",
     region:"south east",
-    email:"dsa",
-    phone:"sdfa",
+    email:"mail@kentdowns.org.uk",
+    phone:"01303 815170",
     image:"https://upload.wikimedia.org/wikipedia/commons/8/82/DownsRanscombeFieldToMway0734c.JPG"
   },
   {
     areaName:"surrey hills",
     description:"the surrey hills is an area located in surrey england and contains woodland, scrub heathland and chalk grassland. it was designated in 1958 and covers an area of 163 square miles. the highest point is leith hill with 965 feet.",
     region:"south east",
-    email:"dddsa",
-    phone:"assds",
+    email:"rob.fairbanks@surreycc.gov.uk",
+    phone:"01372 220653",
     image:"https://upload.wikimedia.org/wikipedia/commons/0/0e/CalvertRoadDorking.jpg"
   },
   {
     areaName:"howardian hills",
     description:"the howardian hills are located in yorkshire and the humber. they take their name from the howard family who still own local lands. the area covers 79 square miles and was designated in 1987. the landscape has a high visual quality through his diverse landform and land us with contrasting scale, colour, texture and form alongside historic houses.",
     region:"yorkshire and humber",
-    email:"acc",
-    phone:"cca",
+    email:"info@howardianhills.org.uk",
+    phone:"0845 034 9495",
     image:"https://upload.wikimedia.org/wikipedia/commons/0/09/Howardian_Hills.jpg"
   },
   {
@@ -287,7 +287,7 @@ var data=[
     description:"nidderdale is located in north yorkshire and includes the attractions brimham rocks and the coldstone cut. it was designated in 1994 and it covers an area of 233 square miles.",
     region:"yorkshire and humber",
     email:"dca",
-    phone:"cca",
+    phone:"01423 712950",
     image:"https://upload.wikimedia.org/wikipedia/commons/3/3b/Upper_Nidderdale.JPG"
   }
 
